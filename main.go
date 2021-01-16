70cdb898a6636e36e3534d80b81adeb0789739cf
0Delta(Bot) <0deltast+bot@gmail.com>
2021-01-16 12:11:53 +0000

    add patched main.go

--- main.go
+++ main.go
@@ -1,11 +1,51 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform/plugin"
-	"github.com/terraform-providers/terraform-provider-google/google"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-provider-google/google"
 )
 
+var seps = "	"
+
+func co(r *schema.Resource, prefix string) {
+	for k, e := range r.Schema {
+		req := ""
+		if e.Required {
+			req = req + "R"
+		}
+		if e.Computed {
+			req = req + "C"
+		}
+		if e.Optional {
+			req = req + "O"
+		}
+		fmt.Printf("%s%s%s%s\n", req, seps, prefix, k)
+		if e.Elem != nil {
+			t, ok := e.Elem.(*schema.Resource)
+			if ok {
+				co(t, prefix+k+seps)
+			}
+		}
+	}
+}
+
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: google.Provider})
+	pf := flag.NewFlagSet("terraform-google-property-exporter", flag.ExitOnError)
+	var (
+		s = pf.String("separator", "	", "separator char")
+		c = pf.Bool("csv", false, "output by csv (overwrite separator option)")
+	)
+	pf.Parse(os.Args[1:])
+	if *c {
+		*s = ","
+	}
+	seps = *s
+	rm := google.ResourceMap()
+	for k, r := range rm {
+		co(r, k+seps)
+	}
 }
