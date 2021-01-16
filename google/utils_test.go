13e13b894b6489cb9770ffe8bb0302c072e9347e
0Delta(Bot) <0deltast+bot@gmail.com>
2021-01-16 12:14:18 +0000

    pull v1.4.0

---
+++ google/utils_test.go
@@ -0,0 +1,201 @@
+package google
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestConvertStringArr(t *testing.T) {
+	input := make([]interface{}, 3)
+	input[0] = "aaa"
+	input[1] = "bbb"
+	input[2] = "aaa"
+
+	expected := []string{"aaa", "bbb", "ccc"}
+	actual := convertStringArr(input)
+
+	if reflect.DeepEqual(expected, actual) {
+		t.Fatalf("(%s) did not match expected value: %s", actual, expected)
+	}
+}
+
+func TestConvertAndMapStringArr(t *testing.T) {
+	input := make([]interface{}, 3)
+	input[0] = "aaa"
+	input[1] = "bbb"
+	input[2] = "aaa"
+
+	expected := []string{"AAA", "BBB", "CCC"}
+	actual := convertAndMapStringArr(input, strings.ToUpper)
+
+	if reflect.DeepEqual(expected, actual) {
+		t.Fatalf("(%s) did not match expected value: %s", actual, expected)
+	}
+}
+
+func TestConvertStringMap(t *testing.T) {
+	input := make(map[string]interface{}, 3)
+	input["one"] = "1"
+	input["two"] = "2"
+	input["three"] = "3"
+
+	expected := map[string]string{"one": "1", "two": "2", "three": "3"}
+	actual := convertStringMap(input)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("%s did not match expected value: %s", actual, expected)
+	}
+}
+
+func TestExtractLastResourceFromUri_withUrl(t *testing.T) {
+	actual := extractLastResourceFromUri("http://something.com/one/two/three")
+	expected := "three"
+	if actual != expected {
+		t.Fatalf("Expected %s, but got %s", expected, actual)
+	}
+}
+
+func TestExtractLastResourceFromUri_WithStaticValue(t *testing.T) {
+	actual := extractLastResourceFromUri("three")
+	expected := "three"
+	if actual != expected {
+		t.Fatalf("Expected %s, but got %s", expected, actual)
+	}
+}
+
+func TestIpCidrRangeDiffSuppress(t *testing.T) {
+	cases := map[string]struct {
+		Old, New          string
+		ExpectDiffSupress bool
+	}{
+		"single ip address": {
+			Old:               "10.2.3.4",
+			New:               "10.2.3.5",
+			ExpectDiffSupress: false,
+		},
+		"cidr format string": {
+			Old:               "10.1.2.0/24",
+			New:               "10.1.3.0/24",
+			ExpectDiffSupress: false,
+		},
+		"netmask same mask": {
+			Old:               "10.1.2.0/24",
+			New:               "/24",
+			ExpectDiffSupress: true,
+		},
+		"netmask different mask": {
+			Old:               "10.1.2.0/24",
+			New:               "/32",
+			ExpectDiffSupress: false,
+		},
+		"add netmask": {
+			Old:               "",
+			New:               "/24",
+			ExpectDiffSupress: false,
+		},
+		"remove netmask": {
+			Old:               "/24",
+			New:               "",
+			ExpectDiffSupress: false,
+		},
+	}
+
+	for tn, tc := range cases {
+		if ipCidrRangeDiffSuppress("ip_cidr_range", tc.Old, tc.New, nil) != tc.ExpectDiffSupress {
+			t.Fatalf("bad: %s, '%s' => '%s' expect %t", tn, tc.Old, tc.New, tc.ExpectDiffSupress)
+		}
+	}
+}
+
+func TestRfc3339TimeDiffSuppress(t *testing.T) {
+	cases := map[string]struct {
+		Old, New          string
+		ExpectDiffSupress bool
+	}{
+		"same time, format changed to have leading zero": {
+			Old:               "2:00",
+			New:               "02:00",
+			ExpectDiffSupress: true,
+		},
+		"same time, format changed not to have leading zero": {
+			Old:               "02:00",
+			New:               "2:00",
+			ExpectDiffSupress: true,
+		},
+		"different time, both without leading zero": {
+			Old:               "2:00",
+			New:               "3:00",
+			ExpectDiffSupress: false,
+		},
+		"different time, old with leading zero, new without": {
+			Old:               "02:00",
+			New:               "3:00",
+			ExpectDiffSupress: false,
+		},
+		"different time, new with leading zero, oldwithout": {
+			Old:               "2:00",
+			New:               "03:00",
+			ExpectDiffSupress: false,
+		},
+		"different time, both with leading zero": {
+			Old:               "02:00",
+			New:               "03:00",
+			ExpectDiffSupress: false,
+		},
+	}
+	for tn, tc := range cases {
+		if rfc3339TimeDiffSuppress("time", tc.Old, tc.New, nil) != tc.ExpectDiffSupress {
+			t.Errorf("bad: %s, '%s' => '%s' expect DiffSuppress to return %t", tn, tc.Old, tc.New, tc.ExpectDiffSupress)
+		}
+	}
+}
+
+func TestGetZone(t *testing.T) {
+	d := schema.TestResourceDataRaw(t, resourceComputeDisk().Schema, map[string]interface{}{
+		"zone": "foo",
+	})
+	var config Config
+	if err := d.Set("zone", "foo"); err != nil {
+		t.Fatalf("Cannot set zone: %s", err)
+	}
+	if zone, err := getZone(d, &config); err != nil || zone != "foo" {
+		t.Fatalf("Zone '%s' != 'foo', %s", zone, err)
+	}
+	config.Zone = "bar"
+	if zone, err := getZone(d, &config); err != nil || zone != "foo" {
+		t.Fatalf("Zone '%s' != 'foo', %s", zone, err)
+	}
+	d.Set("zone", "")
+	if zone, err := getZone(d, &config); err != nil || zone != "bar" {
+		t.Fatalf("Zone '%s' != 'bar', %s", zone, err)
+	}
+	config.Zone = ""
+	if zone, err := getZone(d, &config); err == nil || zone != "" {
+		t.Fatalf("Zone '%s' != '', err=%s", zone, err)
+	}
+}
+
+func TestGetRegion(t *testing.T) {
+	d := schema.TestResourceDataRaw(t, resourceComputeDisk().Schema, map[string]interface{}{
+		"zone": "foo",
+	})
+	var config Config
+	barRegionName := getRegionFromZone("bar")
+	fooRegionName := getRegionFromZone("foo")
+
+	if region, err := getRegion(d, &config); err != nil || region != fooRegionName {
+		t.Fatalf("Zone '%s' != '%s', %s", region, fooRegionName, err)
+	}
+
+	config.Zone = "bar"
+	d.Set("zone", "")
+	if region, err := getRegion(d, &config); err != nil || region != barRegionName {
+		t.Fatalf("Zone '%s' != '%s', %s", region, barRegionName, err)
+	}
+	config.Region = "something-else"
+	if region, err := getRegion(d, &config); err != nil || region != config.Region {
+		t.Fatalf("Zone '%s' != '%s', %s", region, config.Region, err)
+	}
+}
---
+++ website/docs/r/compute_backend_bucket.html.markdown
@@ -0,0 +1,63 @@
+---
+layout: "google"
+page_title: "Google: google_compute_backend_bucket"
+sidebar_current: "docs-google-compute-backend-bucket"
+description: |-
+  Creates a Backend Bucket resource for Google Compute Engine.
+---
+
+# google\_compute\_backend\_bucket
+
+A Backend Bucket defines a Google Cloud Storage bucket that will serve traffic through Google Cloud
+Load Balancer. For more information see
+[the official documentation](https://cloud.google.com/compute/docs/load-balancing/http/backend-bucket)
+and
+[API](https://cloud.google.com/compute/docs/reference/latest/backendBuckets).
+
+## Example Usage
+
+```hcl
+resource "google_compute_backend_bucket" "image_backend" {
+  name        = "image-backend-bucket"
+  description = "Contains beautiful images"
+  bucket_name = "${google_storage_bucket.image_bucket.name}"
+  enable_cdn  = true
+}
+
+resource "google_storage_bucket" "image_bucket" {
+  name     = "image-store-bucket"
+  location = "EU"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required) The name of the backend bucket.
+
+* `bucket_name` - (Required) The name of the Google Cloud Storage bucket to be used as a backend
+    bucket.
+
+- - -
+
+* `description` - (Optional) The textual description for the backend bucket.
+
+* `enable_cdn` - (Optional) Whether or not to enable the Cloud CDN on the backend bucket.
+
+* `project` - (Optional) The project in which the resource belongs. If it is not provided, the
+    provider project is used.
+
+## Attributes Reference
+
+In addition to the arguments listed above, the following computed attributes are exported:
+
+* `self_link` - The URI of the created resource.
+
+## Import
+
+Backend buckets can be imported using the `name`, e.g.
+
+```
+$ terraform import google_compute_backend_bucket.image_backend image-backend-bucket
+```
