cc1d4709ccca1f4780c28305327933a92e387809
0Delta <0deltast@gmail.com>
2021-01-16 20:57:24 +0900

    add flags and change format

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-google/google"
 )
+
+var seps = "	"
 
 func co(r *schema.Resource, prefix string) {
 	for k, e := range r.Schema {
@@ -19,19 +23,29 @@ func co(r *schema.Resource, prefix string) {
 		if e.Optional {
 			req = req + "O"
 		}
-		fmt.Printf("%s  %s%s\n", req, prefix, k)
+		fmt.Printf("%s%s%s%s\n", req, seps, prefix, k)
 		if e.Elem != nil {
 			t, ok := e.Elem.(*schema.Resource)
 			if ok {
-				co(t, prefix+k+"	")
+				co(t, prefix+k+seps)
 			}
 		}
 	}
 }
 
 func main() {
+	pf := flag.NewFlagSet("terraform-google-property-exporter", flag.ExitOnError)
+	var (
+		s = pf.String("separator", "	", "separator char")
+		c = pf.Bool("csv", false, "output by csv (overwrite separator option)")
+	)
+	pf.Parse(os.Args[1:])
+	if *c {
+		*s = ","
+	}
+	seps = *s
 	rm := google.ResourceMap()
 	for k, r := range rm {
-		co(r, k+"	")
+		co(r, k+seps)
 	}
 }
