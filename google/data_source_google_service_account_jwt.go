3d641bf2ed5edf5e71d03c6e7fd44a6c228a24b2
0Delta(Bot) <0deltast+bot@gmail.com>
2023-07-04 02:54:52 +0000

    pull v4.72.0

---
+++ google/data_source_google_service_account_jwt.go
@@ -0,0 +1,9 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package google
+
+import "github.com/hashicorp/terraform-provider-google/google/services/resourcemanager"
+
+var (
+	dataSourceGoogleServiceAccountJwtNow = resourcemanager.DataSourceGoogleServiceAccountJwtNow
+)
