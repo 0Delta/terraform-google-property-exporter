7e968040cf1d4fad85e373c94e33a202db1e5ea6
0Delta(Bot) <0deltast+bot@gmail.com>
2023-07-07 02:53:01 +0000

    pull v4.72.1

---
+++ google/data_source_cloud_identity_groups_test.go
@@ -0,0 +1,46 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package google
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-provider-google/google/acctest"
+	"github.com/hashicorp/terraform-provider-google/google/envvar"
+)
+
+func testAccDataSourceCloudIdentityGroups_basicTest(t *testing.T) {
+
+	context := map[string]interface{}{
+		"org_domain":    envvar.GetTestOrgDomainFromEnv(t),
+		"cust_id":       envvar.GetTestCustIdFromEnv(t),
+		"random_suffix": RandString(t, 10),
+	}
+
+	VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudIdentityGroupConfig(context),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.google_cloud_identity_groups.groups",
+						"groups.#"),
+					resource.TestMatchResourceAttr("data.google_cloud_identity_groups.groups",
+						"groups.0.name", regexp.MustCompile("^groups/.*$")),
+				),
+			},
+		},
+	})
+}
+
+func testAccCloudIdentityGroupConfig(context map[string]interface{}) string {
+	return testAccCloudIdentityGroup_cloudIdentityGroupsBasicExample(context) + Nprintf(`
+
+data "google_cloud_identity_groups" "groups" {
+  parent = google_cloud_identity_group.cloud_identity_group_basic.parent
+}
+`, context)
+}
