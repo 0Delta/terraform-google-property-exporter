893317b712268fbcd4d78e6d282111e6550690c8
0Delta(Bot) <0deltast+bot@gmail.com>
2023-07-11 02:46:38 +0000

    pull v4.73.0

---
+++ google/resource_cloudfunctions_function.go
@@ -0,0 +1,9 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+package google
+
+import (
+	"github.com/hashicorp/terraform-provider-google/google/services/cloudfunctions"
+)
+
+type cloudFunctionId = cloudfunctions.CloudFunctionId
