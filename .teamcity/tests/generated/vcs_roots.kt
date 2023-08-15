787304a3becc54788fcd13c8a6b12ee2d20ff877
0Delta(Bot) <0deltast+bot@gmail.com>
2023-08-15 02:13:18 +0000

    pull v4.78.0

---
+++ .teamcity/tests/generated/vcs_roots.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// this file is auto-generated with mmv1, any changes made here will be overwritten
+
+package tests
+
+import Google
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class VcsTests {
+    @Test
+    fun buildsHaveCleanCheckOut() {
+        val project = Google("public",  TestVcsRootId(), "refs/heads/main", TestConfiguration())
+        project.buildTypes.forEach { bt ->
+            assertTrue("Build '${bt.id}' doesn't use clean checkout", bt.vcs.cleanCheckout)
+        }
+    }
+}
